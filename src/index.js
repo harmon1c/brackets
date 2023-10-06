429bdf5b070ac5fdea39f1ce5578b73e0e214e74
harmon1c <80057115+harmon1c@users.noreply.github.com>
2023-10-06 13:25:44 +0300

    feat: add solution 2023-10-06 13:25:26

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  // Создаем пустой стек (для отслеживания скобок)
+  const stack = [];
+  // Преобразуем конфигурацию скобок в объект для более удобного доступа к парам скобок
+  const bracketPairs = Object.fromEntries(bracketsConfig);
+  // Создаем множество открывающих скобок для более быстрой проверки, входит ли символ в этот список
+  const openBrackets = new Set(Object.keys(bracketPairs));
+  // Создаем множество закрывающих скобок для аналогичной проверки
+  const closeBrackets = new Set(Object.values(bracketPairs));
+  // Проходим по каждому символу в строке
+  for (const char of str) {
+    if (openBrackets.has(char)) {
+      // Если текущий символ - открывающая скобка, проверяем специальные случаи
+      if (closeBrackets.has(char) && stack[stack.length - 1] === char) {
+        stack.pop(); // Если текущая скобка такая же, как предыдущая, удаляем предыдущую из стека
+      } else {
+        stack.push(char); // В противном случае добавляем текущую скобку в стек
+      }
+    } else if (closeBrackets.has(char)) {
+      // Если текущий символ - закрывающая скобка, проверяем соответствие с вершиной стека
+      const lastBracket = stack.pop();
+      if (bracketPairs[lastBracket] !== char) {
+        return false; // Несоответствие пары скобок, возвращаем false
+      }
+    }
+  }
+  // Если стек пуст, это означает, что все скобки правильно закрыты
+  return stack.length === 0;
 }
+
+//Изучить работу со стеком, массивы, объекты.
